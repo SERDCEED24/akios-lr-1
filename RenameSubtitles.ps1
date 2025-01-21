262cef89c5fa94a62795d0f5b3b0ca65fa2e8c31
SERDCEED24 <heraklakra@gmail.com>
2025-01-22 01:22:52 +0500

    Fixed spacing.

--- RenameSubtitles.ps1
+++ RenameSubtitles.ps1
@@ -4,16 +4,16 @@ param (
 
 try {
     $videoFiles = Get-ChildItem -Path $source_dir -Filter "*.mkv"
-$subtitleFiles = Get-ChildItem -Path $source_dir -Filter "*.ass"
+    $subtitleFiles = Get-ChildItem -Path $source_dir -Filter "*.ass"
 
-if ($videoFiles.Count -ne $subtitleFiles.Count) {
-    throw "The number of subtitle files and the number of video files are different! Check your directory, please."
-}
+    if ($videoFiles.Count -ne $subtitleFiles.Count) {
+        throw "The number of subtitle files and the number of video files are different! Check your directory, please."
+    }
 
-for ($i = 0; $i -lt $videoFiles.Count; $i++) {
-    $videoName = $videoFiles[$i].BaseName
-    Rename-Item -Path $subtitleFiles[$i].FullName -NewName "$videoName.ass"
-}
+    for ($i = 0; $i -lt $videoFiles.Count; $i++) {
+        $videoName = $videoFiles[$i].BaseName
+        Rename-Item -Path $subtitleFiles[$i].FullName -NewName "$videoName.ass"
+    }
 }
 catch {
     Write-Output "Error! Check the path to desired directory."
