27b5028fa575c4222db9d20fdbcf4d77a7ca1fe8
SERDCEED24 <heraklakra@gmail.com>
2025-01-22 01:14:29 +0500

    Found errors and eliminated them.

--- RenameSubtitles.ps1
+++ RenameSubtitles.ps1
@@ -4,19 +4,17 @@ param (
 
 try {
     $videoFiles = Get-ChildItem -Path $source_dir -Filter "*.mkv"
-    $subtitleFiles = Get-ChildItem -Path $source_dir -Filter "*.ass"
+$subtitleFiles = Get-ChildItem -Path $source_dir -Filter "*.ass"
 
-    if ($videoFiles.Count -ne $subtitleFiles.Count) {
-        throw "Количество субтитров и видео файлов не совпадают. Проверьте директорию с файлами."
-    }
+if ($videoFiles.Count -ne $subtitleFiles.Count) {
+    throw "The number of subtitle files and the number of video files are different! Check your directory, please."
+}
 
-    for ($i = 0; $i -lt $videoFiles.Count; $i++){
-        $videoName = $videoFiles[$i].BaseName
-        Rename-Item -Path $subtitleFiles.FullName -NewName "$videoName.ass"
-    }
+for ($i = 0; $i -lt $videoFiles.Count; $i++) {
+    $videoName = $videoFiles[$i].BaseName
+    Rename-Item -Path $subtitleFiles[$i].FullName -NewName "$videoName.ass"
+}
 }
 catch {
-    Write-Output "В ходе выполнения возникла ошибка! Проверьте что путь до папки с файлами указан верно."
+    Write-Output "Error! Check the path to desired directory."
 }
-
-Write-Output "Субтитры успешно переименованы."
